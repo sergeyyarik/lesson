72175413e7ec01e7e3eab88f6df025de39a379b6
Sergey <67259702+sergeyyarik@users.noreply.github.com>
2026-01-11 21:30:42 +0200

    Delete lesson_18 directory

--- lesson_18/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('click', documentActions);
-
-function documentActions(e) {
-    const targetElement = e.target;
-    if (targetElement.closest('.header-yoga__burger')) {
-        document.documentElement.toggleAttribute('data-menu-open');
-        console.log('Menu toggle');
-    }
-}
